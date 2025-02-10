8326a97c1ba777407b18ccad85a6a5f6fe4ee529
seung-yuppy <songsy0107@gmail.com>
2025-02-10 10:22:31 +0900

    25/02/10 keys변경

--- src/services/api.js
+++
@@ -1,9 +0,0 @@
-const API = {
-    OMDB_URL: 'https://www.omdbapi.com/',
-    TMDB_URL: 'https://api.themoviedb.org/3/movie/',
-    OMDB_API_KEY: '2326d4aa',
-    TMDB_API_KEY: '779e364663078556f03390357669f7ce',
-    TMDB_ACCESS_TOKEN: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NzllMzY0NjYzMDc4NTU2ZjAzMzkwMzU3NjY5ZjdjZSIsIm5iZiI6MTcyMzk2MjQ1NS45MjcsInN1YiI6IjY2YzE5NDU3ZTg0MGNhMmViZDY3M2YyMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.IXRoxEPc2YTf7HzgBLdHNKqTyQN2KHi3tGcw4IVe5EU',
-}
-
-export default API;
